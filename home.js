b8c830e6298a86e2ce8c404d79038988408fd049
winterbelle <blancaaltamirano@pursuit.org>
2023-04-04 04:57:05 -0400

    home page created and functional

---
+++ home.js
@@ -0,0 +1,13 @@
+// console.log("Hello World"); //making sure this script is linked to the html page.
+
+window.onload = () => {
+    document.getElementsByClassName("pick-starter")[0].onclick = () => {
+        window.open("pick-starter.html", "_self");
+    }
+    document.getElementsByClassName("pokedex")[0].onclick = () => {
+        window.open("pokedex.html", "_self");
+    }
+    document.getElementsByClassName("about")[0].onclick = () => {
+        window.open("about-dev.html", "_self");
+    }
+}
